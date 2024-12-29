d6029531337992576a30afa54e2d09f2f1759153
zahid-official <zahid.official8@gmail.com>
2024-12-29 22:35:52 +0600

    Server Initialize

---
+++ Initial Setup/server/index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.rjxsn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // database
+    const database = client.db("foodsDB"); //change DB name as requirement
+    const foodCollection = database.collection("foods"); //change collection as requirement
+
+    // read Operation
+    app.get("/", (req, res) => {
+      res.send("Server Connected Successfully");
+    });
+
+
+  } finally {
+  }
+}
+run().catch(console.log);
+
+// port running on
+app.listen(port, () => {
+  console.log(`Server Running on... : ${port}`);
+});
