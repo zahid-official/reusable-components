2f4657abc7630e431597f47ac41ea23567b3b5cb
zahid-official <zahid.official8@gmail.com>
2024-12-30 15:54:08 +0600

    Rename Folder

--- Initial Setup/client/src/Layout/MainLayout.jsx
+++ Initial Setup/client/src/Layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
 
 const MainLayout = () => {
   return (
